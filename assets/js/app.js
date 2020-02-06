fd1bda021309ae77f0450c176cab1a21c1913896
jordan <jordanscheftic@yahoo.com>
2020-02-05 19:22:27 -0800

    added files

---
+++ assets/js/app.js
@@ -0,0 +1,136 @@
+
+
+
+
+
+
+
+
+
+
+{
+  "location": {
+    "name": "Orange",
+      "region": "California",
+        "country": "United States of America",
+          "lat": 33.79,
+            "lon": -117.85,
+              "tz_id": "America/Los_Angeles",
+                "localtime_epoch": 1580927012,
+                  "localtime": "2020-02-05 10:23"
+  },
+  "current": {
+    "last_updated": "2020-02-05 10:15",
+      "temp_f": 57.9,
+        "is_day": 1,
+          "condition": {
+      "text": "Sunny",
+        "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+          "code": 1000
+    },
+    "wind_mph": 0.0,
+      "humidity": 25,
+        "cloud": 0,
+          "feelslike_f": 57.9,
+            "vis_miles": 9.0,
+              "uv": 4.0,
+                "gust_mph": 5.1
+  },
+  "forecast": {
+    "forecastday": [
+      {
+        "date": "2020-02-05",
+        "day": {
+          "maxtemp_f": 61.2,
+          "mintemp_f": 48.6,
+          "avgtemp_f": 55.9,
+          "avghumidity": 23.0,
+          "condition": {
+            "text": "Partly cloudy",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+            "code": 1003
+          },
+          "uv": 3.3
+        },
+        "astro": {
+
+        }
+      },
+      {
+        "date": "2020-02-06",
+        "day": {
+          "maxtemp_f": 66.4,
+          "mintemp_f": 53.6,
+          "avgtemp_f": 59.7,
+          "avghumidity": 38.0,
+          "condition": {
+            "text": "Partly cloudy",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+            "code": 1003
+          },
+          "uv": 4.1
+        },
+        "astro": {
+
+        }
+      },
+      {
+        "date": "2020-02-07",
+        "day": {
+          "maxtemp_f": 69.8,
+          "mintemp_f": 56.1,
+          "avgtemp_f": 63.3,
+          "avghumidity": 49.0,
+          "condition": {
+            "text": "Sunny",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+            "code": 1000
+          },
+          "uv": 4.4
+        },
+        "astro": {
+
+        }
+      },
+      {
+        "date": "2020-02-08",
+        "day": {
+          "maxtemp_f": 69.8,
+          "mintemp_f": 57.7,
+          "avgtemp_f": 63.2,
+          "avghumidity": 54.0,
+          "condition": {
+            "text": "Partly cloudy",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+            "code": 1003
+          },
+          "uv": 4.3
+        },
+        "astro": {
+
+        }
+      },
+      {
+        "date": "2020-02-09",
+        "day": {
+          "maxtemp_f": 58.8,
+          "mintemp_f": 51.8,
+          "avgtemp_f": 55.0,
+          "avghumidity": 72.0,
+          "condition": {
+            "text": "Moderate or heavy rain shower",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/356.png",
+            "code": 1243
+          },
+          "uv": 3.0
+        },
+        "astro": {
+
+        }
+      }
+    ]
+  },
+  "alert": {
+
+  }
+}
